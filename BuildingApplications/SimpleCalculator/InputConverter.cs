b58f640b1f1a4898802c0f8a0ff94d0f59b77b86
Justice Ngwenya <justice@annotations.co.za>
2021-04-06 10:16:00 +0200

    Simple Calculator

---
+++ BuildingApplications/SimpleCalculator/InputConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertNumber;
+            if (!double.TryParse(argTextInput, out convertNumber))
+                throw new ArgumentException("Expected a numeric value");
+            return convertNumber;
+        }
+
+
+    }
+}
---
+++ BuildingApplications/SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                Console.WriteLine("Please enter first Number");
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Please enter second Number");
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+                Console.WriteLine("Please enter Operation e.g (+, /, *, -)");
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine($"Results = {result}");
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
